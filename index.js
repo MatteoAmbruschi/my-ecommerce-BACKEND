e3b963e6b7d36926c6e43bdbd6709bed6c674247
Matteo <matteoambruschi10@gmail.com>
2024-06-13 16:06:08 +0200

    cambio porta

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const store = new session.MemoryStore();
 const flash = require('connect-flash');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: ['http://localhost:3001', 'http://localhost:3001/', 'http://localhost:3000', 'http://localhost:3000/', 'http://localhost:5173/', 'http://localhost:5173'],
