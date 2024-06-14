a41dd7730f283ab98c77f6257b77c4da84c0e6f3
Matteo <matteoambruschi10@gmail.com>
2024-06-14 22:22:39 +0200

    secure true

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     sameSite: 'lax',
-/*     secure: process.env.NODE_ENV === 'production', */
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
